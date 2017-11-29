d3e5310864b5c7b00c1973a10f45829b7f68912f
prajwalx <prajwalbhati@hotmail.com>
2017-11-29 13:17:10 +0530

    All Posts showing and Single Post Opening

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,19 +7,32 @@
     constructor($http, $scope, socket) {
       this.$http = $http;
       this.socket = socket;
-      this.awesomeThings = [];
+      this.AllPosts=[];
 
-      $scope.$on('$destroy', function() {
-        socket.unsyncUpdates('thing');
-      });
+      this.sngpst=false;
+      this.alpst=true;
+      this.SinglePost;
+      // this.awesomeThings = [];
+      //
+      // $scope.$on('$destroy', function() {
+      //   socket.unsyncUpdates('thing');
+      // });
     }
 
     $onInit() {
-      this.$http.get('/api/things')
-        .then(response => {
-          this.awesomeThings = response.data;
-          this.socket.syncUpdates('thing', this.awesomeThings);
-        });
+      // this.$http.get('/api/things')
+      //   .then(response => {
+      //     this.awesomeThings = response.data;
+      //     this.socket.syncUpdates('thing', this.awesomeThings);
+      //   });
+
+      this.$http.get('http://assignment-server.herokuapp.com/posts')
+          .then(response =>{
+            this.AllPosts=response.data;
+
+            this.socket.syncUpdates('http://assignment-server.herokuapp.com/posts',this.AllPosts);
+            console.log(this.AllPosts);
+          });
     }
 
     addThing() {
@@ -31,14 +44,29 @@
       }
     }
 
+
     deleteThing(thing) {
       this.$http.delete('/api/things/' + thing._id);
+    }
+
+    Display(x){
+      console.log(x);
+      this.alpst=false;
+      this.sngpst=true;
+      this.SinglePost=x;
+
+    }
+    closeSngpst(){
+      this.alpst=true;
+      this.sngpst=false;
+
     }
   }
 
   angular.module('bloggingApplicationApp')
     .component('main', {
       templateUrl: 'app/main/main.html',
-      controller: MainController
+      controller: MainController,
+      controllerAs:'mainCtrl'
     });
 })();
