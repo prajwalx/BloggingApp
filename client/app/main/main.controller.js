5ba8ccae743e435b274b0d91bbf27b7a8fc95584
prajwalx <prajwalbhati@hotmail.com>
2017-11-29 17:57:20 +0530

    Comment Section completed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,6 +12,9 @@
       this.sngpst=false;
       this.alpst=true;
       this.SinglePost;
+      this.comments=[];
+      this.TextComment="";
+      this.cmtCls;
       // this.awesomeThings = [];
       //
       // $scope.$on('$destroy', function() {
@@ -26,13 +29,28 @@
       //     this.socket.syncUpdates('thing', this.awesomeThings);
       //   });
 
+      //initialise comment class
+      this.cmtCls="media-body input-group form-group has-feedback";
+
+      //Get Posts
       this.$http.get('http://assignment-server.herokuapp.com/posts')
           .then(response =>{
             this.AllPosts=response.data;
 
-            this.socket.syncUpdates('http://assignment-server.herokuapp.com/posts',this.AllPosts);
+            //this.socket.syncUpdates('http://assignment-server.herokuapp.com/posts',this.AllPosts);
             console.log(this.AllPosts);
           });
+      //Get Comments
+      this.$http.get('http://assignment-server.herokuapp.com/comments')
+              .then(response =>{
+                this.comments=response.data;
+
+                //this.socket.syncUpdates('http://assignment-server.herokuapp.com/comments',this.comments);
+                console.log((this.comments));
+              });
+
+
+
     }
 
     addThing() {
@@ -49,18 +67,47 @@
       this.$http.delete('/api/things/' + thing._id);
     }
 
+    topup(){
+      document.body.scrollTop=0;
+      document.documentElement.scrollTop=0;
+    }
     Display(x){
       console.log(x);
       this.alpst=false;
       this.sngpst=true;
       this.SinglePost=x;
-
+      this.topup();
     }
     closeSngpst(){
       this.alpst=true;
       this.sngpst=false;
 
     }
+    AddComment(){
+      this.cmtCls="media-body input-group form-group has-feedback";
+      console.log(this.TextComment);
+      console.log(this.SinglePost.id);
+      //If string is not Empty
+      if(this.TextComment!==""){
+
+          this.$http.post('http://assignment-server.herokuapp.com/comments',{
+            body:this.TextComment,
+            postId:this.SinglePost.id
+          }).then(response=>{
+            this.TextComment="";
+            console.log('Comments Added Hopefully Let"s See');
+            console.log(response.data);
+            this.cmtCls="media-body input-group form-group has-feedback has-success";
+            this.comments.push(response.data);
+          });
+    }
+    else{
+      this.cmtCls="media-body input-group form-group has-feedback has-error";
+      console.log('Empty String');
+    }
+      //this.cmtCls="media-body input-group form-group has-feedback";
+    }
+
   }
 
   angular.module('bloggingApplicationApp')
